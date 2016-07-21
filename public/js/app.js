02a31722446e3e95c9c4171a64c85a775b700cc4
roys <roy.shell@netomedia.com>
2016-07-21 11:45:07 +0300

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to socket.io to the server');
-})
+});
+
+socket.on('message', function (message) {
+	console.log('New message:');
+	console.log(message.text);
+});
